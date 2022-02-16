e43c8ed2f400a6d371ad787acf444e5de3ddb4dd
Roselle Wang <wrh0919@gmail.com>
2022-02-16 01:27:03 -0800

    Created LongestInfo class

---
+++ LongestInfo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class LongestInfo {
+    int length;
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<Integer> offset = new ArrayList<>();
+    String hashString;
+
+    public LongestInfo(String fileOne, String fileTwo, int offsetOne, int offsetTwo, int len, String result){
+        names.add(fileOne);
+        
+    }
+}
