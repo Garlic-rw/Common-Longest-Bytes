06dd8ce0e63902795c84555016c839a1fdf41876
Roselle Wang <wrh0919@gmail.com>
2022-02-16 16:29:37 -0800

    added update function in LongestInfor class

--- LongestInfo.java
+++ LongestInfo.java
@@ -4,10 +4,42 @@ public class LongestInfo {
     int length;
     ArrayList<String> names = new ArrayList<>();
     ArrayList<Integer> offset = new ArrayList<>();
-    String hashString;
+    int hashString;
 
     public LongestInfo(String fileOne, String fileTwo, int offsetOne, int offsetTwo, int len, String result){
         names.add(fileOne);
-        
+        names.add(fileTwo);
+        offset.add(offsetOne);
+        offset.add(offsetTwo);
+        length = len;
+        hashString = result.hashCode();
+    }
+    
+    public LongestInfo() {
+        this.length = 0;
+        this.hashString = 0;
+    }
+
+    public LongestInfo update(String fileOne, String fileTwo, int offsetOne, int offsetTwo, int len, String result) {
+        if (this.length > len) {
+            return this;
+        } else if (this.length < len) {
+            return new LongestInfo(fileOne, fileTwo, offsetOne, offsetTwo, len, result);
+        } else {
+            if (this.hashString != result.hashCode()) {
+                return this;
+            } else {
+                if (!names.contains(fileOne)){
+                    names.add(fileOne);
+                    offset.add(offsetOne);
+                }
+                if(!names.contains(fileTwo)){
+                    names.add(fileTwo);
+                    offset.add(offsetTwo);
+                }
+                return this;
+            }
+        }
     }
 }
+
