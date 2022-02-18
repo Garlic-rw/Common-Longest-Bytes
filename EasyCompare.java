585e8ea910827c77d4391a5d95faedbbc13884f8
Roselle Wang <wrh0919@gmail.com>
2022-02-17 21:52:40 -0800

    Added longestCommonStringFast function, able to work with big files

--- EasyCompare.java
+++ EasyCompare.java
@@ -52,11 +52,58 @@ public class EasyCompare{
         return new LongestInfo(one.fileName, two.fileName, firstOffset, secondOffset, longest, rt.hashCode());
     }
 
+    public static LongestInfo longestCommonStringFast(EasyCompare one, EasyCompare two) {
+        String first = one.hexString;
+        String second = two.hexString;
+        int[][] common = new int[2][second.length()/2];
+        String rt = "";
+        int longest = 0;
+        int firstOffset = 0;
+        int secondOffset = 0;
+        for (int j = 0; j < second.length(); j += 2) {
+            if (first.substring(0, 2).equals(second.substring(j, j+2))){
+                common[0][j/2] = 2;
+                if (common[0][j/2] > longest) {
+                    longest = common[0][j/2];
+                    rt = first.substring(0-longest+2, 0+2); 
+                    firstOffset = 0 - longest + 2;
+                    secondOffset = j - longest + 2;
+                } 
+            }
+            else{
+                common[0][j/2] = 0;
+            }
+        }
+        for (int i = 2; i< first.length(); i += 2) {
+            for (int j = 0; j < second.length(); j += 2){
+                if (first.substring(i, i + 2).equals(second.substring(j, j+2))){
+                    if (i == 0 || j == 0) {
+                        common[1][j/2] = 2;
+                    } else {
+                        common[1][j/2] = common[0][j/2 - 1] + 2;
+                    }
+                    if (common[1][j/2] > longest) {
+                        longest = common[1][j/2];
+                        rt = first.substring(i-longest+2, i+2); 
+                        firstOffset = i - longest + 2;
+                        secondOffset = j - longest + 2;
+                    } 
+                }
+                else{
+                    common[1][j/2] = 0;
+                }
+            }
+            common[0] = common[1];
+            common[1] = new int[second.length()/2];
+        }
+        return new LongestInfo(one.fileName, two.fileName, firstOffset, secondOffset, longest, rt.hashCode());
+    }
+
     public static LongestInfo longestCommonFull(EasyCompare[] input) {
         LongestInfo rt = new LongestInfo();
         for (int i = 0; i < input.length; i++) {
             for (int j = i + 1; j < input.length; j++){
-                LongestInfo compare = longestCommonString(input[i], input[j]);
+                LongestInfo compare = longestCommonStringFast(input[i], input[j]);
                 rt = rt.update(compare.names.get(0), compare.names.get(1), compare.offset.get(0), compare.offset.get(1), compare.length, compare.hashString);
             }
         }
@@ -74,22 +121,23 @@ public static void main(String[] args) throws IOException {
     EasyCompare test8 = new EasyCompare("samples/sample.8", "sample.8");
     EasyCompare test9 = new EasyCompare("samples/sample.9", "sample.9");
     EasyCompare test10 = new EasyCompare("samples/sample.10", "sample.10");
-    EasyCompare[] test = new EasyCompare[3];
-    //test[0] = test1;
-    //test[1] = test2;
-    //test[2] = test3;
-    //test[3] = test4;
-    //test[4] = test5;
-    //test[5] = test6;
-    //test[6] = test7;
-    test[0] = test8;
-    test[1] = test9;
-    test[2] = test10;
+    EasyCompare[] test = new EasyCompare[10];
+    test[0] = test1;
+    test[1] = test2;
+    test[2] = test3;
+    test[3] = test4;
+    test[4] = test5;
+    test[5] = test6;
+    test[6] = test7;
+    test[7] = test8;
+    test[8] = test9;
+    test[9] = test10;
     LongestInfo result = longestCommonFull(test);
     System.out.println(result.length);
     System.out.println(result.names);
     System.out.println(result.offset);
-    //System.out.println(longestCommonString(test9, test10).length);
+    //System.out.println(longestCommonStringFast(test8, test9).length);
+    
 
 }
 }
